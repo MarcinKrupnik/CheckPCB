c0d08487ed114098f1b61cbf726d519ebab3bb91
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-11-26 20:34:13 +0100

    Changes

---
+++ text_recognition.py
@@ -0,0 +1,12 @@
+import cv2
+import matplotlib.pyplot as plt
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+img = cv2.imread('your_image2.jpg') # wczytanie obrazka
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # konwersja na czarno białe
+thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)[1] # wszystkie wartości powyżej 127 zamieniane na 255(biały)
+#plt.imshow(thresh, 'gray')
+#plt.show() # wyświetlanie obrazka
+
+output = pytesseract.image_to_string(thresh, lang='eng', config='--psm 11')
+print('Output: ', output)
