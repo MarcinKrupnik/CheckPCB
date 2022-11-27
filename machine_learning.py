67ea65dc3ae380c211f83348a1c8fc645e8552cf
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-11-27 13:21:39 +0100

    Ckeck image

--- machine_learning.py
+++ machine_learning.py
@@ -1,13 +1,17 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="x8OJScIiVtQRdqBexntp")
-project = rf.workspace().project("smdcomponents")
-model = project.version(6).model
+def main():
+    rf = Roboflow(api_key="x8OJScIiVtQRdqBexntp")
+    project = rf.workspace().project("smdcomponents")
+    model = project.version(6).model
 
-# infer on a local image
-print(model.predict("your_image.jpg", confidence=65, overlap=15).json())
+    # infer on a local image
+    print(model.predict("your_image.jpg", confidence=65, overlap=15).json())
 
-# visualize your prediction
-model.predict("your_image.jpg", confidence=65, overlap=15).save("prediction.jpg")
+    # visualize your prediction
+    model.predict("your_image.jpg", confidence=65, overlap=15).save("prediction.jpg")
 
-# infer on an image hosted elsewhere
-# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+    # infer on an image hosted elsewhere
+    # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+
+if __name__ == "__main__":
+    main()
