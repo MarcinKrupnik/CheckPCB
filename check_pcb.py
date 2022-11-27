15ba4c70b3bc59105e9ba8be8ae9a3545cd66c98
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-11-27 01:56:25 +0100

    Some

--- check_pcb.py
+++ check_pcb.py
@@ -1,15 +1,167 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt 
+from PyQt5.QtWidgets import QMainWindow, QApplication, QSizeGrip, QGraphicsDropShadowEffect
+from PyQt5.QtCore import QPropertyAnimation
+from PyQt5.QtGui import QColor
 import sys
+from PySide2 import *
+from ui import *
+#import machine_learning
+#import text_recognition
 
-class UI(QMainWindow):
-        def __init__(self):
-                super(UI, self).__init__()
+########################################################################
+## MAIN WINDOW CLASS
+########################################################################
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-                uic.loadUi("check_pcb.ui", self)
-                self.show()
+        #######################################################################
+        ## # Remove window tittle bar
+        ########################################################################    
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint) 
 
-app = QApplication(sys.argv)
-UIWindow = UI()
-app.exec_()
+        #######################################################################
+        ## # Set main background to transparent
+        ########################################################################  
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+      
+        #######################################################################
+        ## # Shadow effect style
+        ########################################################################  
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(50)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 92, 157, 550))
+        
+        #######################################################################
+        ## # Appy shadow to central widget
+        ########################################################################  
+        self.ui.centralwidget.setGraphicsEffect(self.shadow)
+
+        #######################################################################
+        # Set window Icon
+        # This icon and title will not appear on our app main window because we removed the title bar
+        #######################################################################
+        self.setWindowIcon(QtGui.QIcon("icons/check-circle.svg"))
+        # Set window tittle
+        self.setWindowTitle("Check PCB")
+
+        #################################################################################
+        # Window Size grip to resize window
+        #################################################################################
+        #QSizeGrip(self.ui.size_grip)
+
+        #######################################################################
+        #Minimize window
+        self.ui.minimize_window_button.clicked.connect(lambda: self.showMinimized())
+        #######################################################################
+        #Close window
+        self.ui.close_window_button.clicked.connect(lambda: self.close())
+        #self.ui.exit_button.clicked.connect(lambda: self.close())
+
+
+        #######################################################################
+        #Restore/Maximize window
+        self.ui.restore_window_button.clicked.connect(lambda: self.restore_or_maximize_window())
+
+
+        # ###############################################
+        # Function to Move window on mouse drag event on the tittle bar
+        # ###############################################
+        def moveWindow(e):
+            # Detect if the window is  normal size
+            # ###############################################  
+            if self.isMaximized() == False: #Not maximized
+                # Move window only when window is normal size  
+                # ###############################################
+                #if left mouse button is clicked (Only accept left mouse button clicks)
+                if e.buttons() == QtCore.Qt.LeftButton:  
+                    #Move window 
+                    self.move(self.pos() + e.globalPos() - self.clickPosition)
+                    self.clickPosition = e.globalPos()
+                    e.accept()
+        #######################################################################
+
+        #######################################################################
+        # Add click event/Mouse move event/drag event to the top header to move the window
+        #######################################################################
+        self.ui.header_frame.mouseMoveEvent = moveWindow
+        #######################################################################
+
+
+        #######################################################################
+        #Left Menu toggle button
+        self.ui.menu_button.clicked.connect(lambda: self.slideLeftMenu())
+
+
+        self.show()
+
+
+
+
+    ########################################################################
+    # Slide left menu function
+    ########################################################################
+    def slideLeftMenu(self):
+        # Get current left menu width
+        width = self.ui.menu_frame.width()
+
+        # If minimized
+        if width == 0:
+            # Expand menu
+            newWidth = 200
+            self.ui.menu_button.setIcon(QtGui.QIcon(u"icons/chevron-left.svg"))
+        # If maximized
+        else:
+            # Restore menu
+            newWidth = 0
+            self.ui.menu_button.setIcon(QtGui.QIcon(u"icons/align-left.svg"))
+
+        #Animate the transition
+        self.animation = QPropertyAnimation(self.ui.menu_frame, b"maximumWidth")#Animate minimumWidht
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)#Start value is the current menu width
+        self.animation.setEndValue(newWidth)#end value is the new menu width
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
+    #######################################################################
+
+
+
+
+
+    #######################################################################
+    # Add mouse events to the window
+    #######################################################################
+    def mousePressEvent(self, event):
+        # ###############################################
+        # Get the current position of the mouse
+        self.clickPosition = event.globalPos()
+        # We will use this value to move the window
+    #######################################################################
+    #######################################################################
+
+
+
+    #######################################################################
+    # Update restore button icon on msximizing or minimizing window
+    #######################################################################
+    def restore_or_maximize_window(self):
+        # If window is maxmized
+        if self.isMaximized():
+            self.showNormal()
+            # Change Icon
+            self.ui.restore_window_button.setIcon(QtGui.QIcon(u"icons/maximize-2.svg"))
+        else:
+            self.showMaximized()
+            # Change Icon
+            self.ui.restore_window_button.setIcon(QtGui.QIcon(u"icons/minimize-2.svg"))
+
+if __name__ == "__main__":
+        app = QApplication(sys.argv)
+        #UIWindow = UI()
+        window = MainWindow()
+        sys.exit(app.exec_())
--- setup_venv.bat
+++
@@ -1,3 +0,0 @@
-D:\etools\Utils\Python\python-3.8.2\python.exe -m venv venv
-
-venv\Scripts\pip.exe install -r requirements.txt
