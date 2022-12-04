92d6b84febaa2ce90b093bd836cabf9e1347530e
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-12-04 19:14:47 +0100

    repx6

--- make_image.py
+++ make_image.py
@@ -13,8 +13,10 @@ def make_image(self,image_name,base_name):
             os.mkdir(str(os.getcwd())+'/images/'+ image_name+'/'+image_name+'_base')
         os.system('libcamera-jpeg -o '+' images/'+ image_name + '/'+ image_name +'_base'+'/'+image_name +'_base'+'.jpg')
     else:
-        if not os.path.exists('images/'+ image_name+'/'+ image_name):
+        try:
             os.mkdir(str(os.getcwd())+'images/'+ image_name+"/"+image_name)
+        except:
+            print("Folder już istnieje")
         os.system('libcamera-jpeg -o '+' images/'+image_name+'/'+ image_name +'/'+image_name+str(1+len(os.listdir('images/'+base_name+'/'+ image_name)))+'.jpg')
     self.comboBox_prod_update()
 
