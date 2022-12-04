e9c70090ea30a1d68320fc1a47a532af3a3b3186
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-12-04 18:57:15 +0100

    stupid fail

--- make_image.py
+++ make_image.py
@@ -6,11 +6,11 @@ def make_image(self,image_name,base_name):
     if not os.path.exists('/images/'+ image_name):
         os.mkdir(str(os.getcwd())+'/images/'+ image_name)
     if base_name == "base":
-        os.system('libcamera-jpeg -o'+' images/'+ image_name + '/'+ image_name +'_base'+'/'+image_name +'_base'+'.jpg')
+        os.system('libcamera-jpeg -o '+' images/'+ image_name + '/'+ image_name +'_base'+'/'+image_name +'_base'+'.jpg')
     else:
         if not os.path.exists('images/'+ base_name+'/'+ image_name):
             os.mkdir(str(os.getcwd())+'images/'+ image_name)
-        os.system('libcamera-jpeg -o'+' images/'+base_name+'/'+ image_name +'/'+image_name+(1+len(os.listdir('images/'+base_name+'/'+ image_name)))+'.jpg')
+        os.system('libcamera-jpeg -o '+' images/'+base_name+'/'+ image_name +'/'+image_name+(1+len(os.listdir('images/'+base_name+'/'+ image_name)))+'.jpg')
     self.comboBox_prod_update()
 
 
