2114e860689b9e401476c34e3a0e169544d4c69b
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-12-04 18:59:54 +0100

    folder rep

--- make_image.py
+++ make_image.py
@@ -6,6 +6,8 @@ def make_image(self,image_name,base_name):
     if not os.path.exists('/images/'+ image_name):
         os.mkdir(str(os.getcwd())+'/images/'+ image_name)
     if base_name == "base":
+        if not os.path.exists('/images/'+ image_name):
+            os.mkdir(str(os.getcwd())+'/images/'+ image_name+'/'+image_name+'_base')
         os.system('libcamera-jpeg -o '+' images/'+ image_name + '/'+ image_name +'_base'+'/'+image_name +'_base'+'.jpg')
     else:
         if not os.path.exists('images/'+ base_name+'/'+ image_name):
