245adc51e0fbf7b8d8a530cedc82edef82468920
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-12-04 18:42:54 +0100

    rep2

--- make_image.py
+++ make_image.py
@@ -3,8 +3,8 @@ import ui
 def main():
     print("Main")
 def make_image(self,image_name,base_name):
-    if not os.path.exists('images/'+ image_name):
-        os.mkdir(str(os.getcwd())+'images/'+ image_name)
+    if not os.path.exists('/images/'+ image_name):
+        os.mkdir(str(os.getcwd())+'/images/'+ image_name)
     if base_name == "base":
         os.system('libcamera-jpeg -o'+' images/'+ image_name + '/'+ image_name +'_base'+'/'+image_name +'_base'+'.jpeg')
     else:
