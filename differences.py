988ef33ec570e905d0eefd28cebcef66b2eda44f
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-12-04 20:42:52 +0100

    Update differences.py

--- differences.py
+++ differences.py
@@ -6,11 +6,11 @@ def main():
 
 def differences(self):
         self.ui.stackedWidget.setCurrentWidget(self.ui.page_3)
-        image1 = Image.open('images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.difference_combobox.currentText())
-        image2 = Image.open('images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'_base/'+self.ui.choose_base_combobox.currentText()+'.jpg')
+        image1 = Image.open('/images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.difference_combobox.currentText())
+        image2 = Image.open('/images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'_base/'+self.ui.choose_base_combobox.currentText()+'.jpg')
         diff = ImageChops.difference(image1,image2)
-        diff = diff.save('images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'_base/difference.jpg')
-        self.image=QPixmap('images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'_base/difference.jpg')
+        diff = diff.save('/images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'_base/difference.jpg')
+        self.image=QPixmap('/images/'+self.ui.choose_base_combobox.currentText()+'/'+self.ui.choose_base_combobox.currentText()+'_base/difference.jpg')
         self.ui.first_image.setMaximumSize(QtCore.QSize(self.width, self.height))
         self.ui.first_image.setPixmap(self.image.scaled(self.rez))
         #self.ui.frame_4.show()
