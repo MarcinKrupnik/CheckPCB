73f1ca355029ff7a582a57671a403185b5388dc1
MarcinKrupnik <marcinkrupnikmail@gmail.com>
2022-12-04 19:06:15 +0100

    repx3

--- make_image.py
+++ make_image.py
@@ -14,7 +14,7 @@ def make_image(self,image_name,base_name):
         os.system('libcamera-jpeg -o '+' images/'+ image_name + '/'+ image_name +'_base'+'/'+image_name +'_base'+'.jpg')
     else:
         if not os.path.exists('images/'+ base_name+'/'+ image_name):
-            os.mkdir(str(os.getcwd())+'images/'+ image_name)
+            os.mkdir(str(os.getcwd())+'images/'+ image_name+"/"+image_name)
         os.system('libcamera-jpeg -o '+' images/'+base_name+'/'+ image_name +'/'+image_name+str(1+len(os.listdir('images/'+base_name+'/'+ image_name)))+'.jpg')
     self.comboBox_prod_update()
 
